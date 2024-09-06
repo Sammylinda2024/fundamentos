918553932780a2028feb92b9fa71cd638d71e77a
samara lemes <sammylinda2024@hotmail.com>
2024-09-05 21:33:33 -0300

    Add files via upload

---
+++ Lógica de programaçao aula 02/caixa.js
@@ -0,0 +1,12 @@
+
+saldoDisponivel = 100;
+valorDoSaque = prompt("informe o valor do saque:")
+
+if (valorDoSaque <= saldoDisponivel) {
+    saldoDisponivel = saldoDisponivel - valorDoSaque
+    document.write(" voce está sacando r$ ",valorDoSaque,".")
+} else {
+    document.write("o valor solicitado é maior que o valor disponivél para saque!")
+}
+
+document.write("saldo disponivel: R$",saldoDisponivel)
